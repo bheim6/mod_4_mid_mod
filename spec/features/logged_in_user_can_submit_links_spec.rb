4e54a6c85b2c6dfd238ace4a9ef7c9b7b06827c2
Brian Heim <bheim67@gmail.com>
2016-12-19 16:51:57 -0700

    added skeleton for edit test

--- spec/features/logged_in_user_can_submit_links_spec.rb
+++ spec/features/logged_in_user_can_submit_links_spec.rb
@@ -22,7 +22,26 @@ RSpec.feature "Logged in users can submit links" do
     click_on "Submit"
 
     expect(Link.count).to eq(1)
+    expect(user1.links.count).to eq(1)
     # Additionally, all links have a read status that is either true or false. This status will default to false.
     # Submitting an invalid link should result in an error message being displayed that indicated why the user was not able to add the link.
+  end
+
+  scenario "and edit them" do
+    user1 = create(:user)
+    visit '/'
+    click_on "Log In"
+    expect(current_path).to eq('/login')
+    fill_in "Email address", with: "example@example.com"
+    fill_in "Password", with: user1.password
+    fill_in "Password confirmation", with: user1.password
+    click_on "Submit"
+
+    expect(current_path).to eq('/')
+
+    fill_in "Url", with: "www.google.com"
+    fill_in "Title", with: "The Google"
+    click_on "Submit"
+
   end
 end
