12d3880486b835007e2ab24529c882b0488ff34c
Brian Heim <bheim67@gmail.com>
2016-12-19 14:42:17 -0700

    happy path for user signup and redirect to root complete and tested

--- spec/features/user_can_sign_up_and_login_spec.rb
+++ spec/features/user_can_sign_up_and_login_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature "Users can sign up and login" do
-  scenario "unauthenticated users are redirected to login page" do
+  scenario "unauthenticated users are redirected to login page and can sign up" do
     # As an unauthenticated user, when I visit the root of the application, /,
     visit '/'
     # binding.pry
@@ -10,15 +10,15 @@ RSpec.feature "Users can sign up and login" do
     # As an unauthenticated user, if I click "Sign Up", I should be taken to a user form where I can enter an email address, a password, and a password confirmation.
     click_on "Sign Up"
     expect(current_path).to eq('/signup')
-    save_and_open_page
     fill_in "Email address", with: "example@example.com"
     fill_in "Password", with: "password"
-    fill_in "Password Confirmation", with: "password"
-    #
-    # I cannot sign up with an email address that has already been used.
-    # Password and confirmation must match.
-    # If criteria is not met the user should be given a message to reflect the reason they could not sign up.
+    fill_in "Password confirmation", with: "password"
+    click_on "Submit"
     # Upon submitting this information, I should be logged in via a session cookie and redirected to the "Links Index" page.
-
+    expect(current_path).to eq('/')
   end
+
+  # I cannot sign up with an email address that has already been used.
+  # Password and confirmation must match.
+  # If criteria is not met the user should be given a message to reflect the reason they could not sign up.
 end
