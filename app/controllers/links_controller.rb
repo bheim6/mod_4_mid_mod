be407a57db8f48c744c8807f1206582eb55d2c4a
Brian Heim <bheim67@gmail.com>
2016-12-19 16:18:54 -0700

    add ability to create links, now have flash messages rendering as well

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,6 +3,30 @@ class LinksController < ApplicationController
     unless current_user
       redirect_to login_router_path
     end
+
+    @link = Link.new
+  end
+
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new( link_params )
+
+    if @link.save
+      @link.user_id = current_user.id
+      flash[:notice] = "Link Created!"
+      redirect_to links_path
+    else
+      flash[:danger] = "Link Invalid or not all Parameters created"
+    end
+  end
+
+  private
+  def link_params
+    params.require(:link).permit(:url,
+                                 :title)
   end
 
 end
--- spec/features/logged_in_user_can_submit_links_spec.rb
+++ spec/features/logged_in_user_can_submit_links_spec.rb
@@ -14,13 +14,15 @@ RSpec.feature "Logged in users can submit links" do
 
     expect(current_path).to eq('/')
 
-    #
     # The Link model should include:
-    #
     # A valid URL location for the link
+    fill_in "Url", with: "www.google.com"
     # A title for the link
+    fill_in "Title", with: "The Google"
+    click_on "Submit"
+
+    expect(Link.count).to eq(1)
     # Additionally, all links have a read status that is either true or false. This status will default to false.
-    #
     # Submitting an invalid link should result in an error message being displayed that indicated why the user was not able to add the link.
   end
 end
